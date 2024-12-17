91d39f6da71c6ee88db452f8d2c2c834b56e2595
Abdelhalim Elbouaami <abelboua@c2r3p12.1337.ma>
2024-12-17 10:05:40 +0100

    fixing conflicts

--- printf/src/ft_printf.c
+++ printf/src/ft_printf.c
@@ -14,26 +14,26 @@
 
 void	create_fun_list(t_convert *func_list)
 {
-	(*func_list)[0].sym = "s";
-	(*func_list)[0].f = put_str;
-	(*func_list)[1].sym = "c";
-	(*func_list)[1].f = put_char;
-	(*func_list)[2].sym = "d";
-	(*func_list)[2].f = put_nbr;
-	(*func_list)[3].sym = "i";
-	(*func_list)[3].f = put_nbr;
-	(*func_list)[4].sym = "x";
-	(*func_list)[4].f = call_low_hex;
-	(*func_list)[5].sym = "X";
-	(*func_list)[5].f = call_upp_hex;
-	(*func_list)[6].sym = "u";
-	(*func_list)[6].f = put_unisgned_nbr;
-	(*func_list)[7].sym = "p";
-	(*func_list)[7].f = print_pointer;
-	(*func_list)[8].sym = "%";
-	(*func_list)[8].f = print_space;
-	(*func_list)[9].sym = 0;
-	(*func_list)[9].f = NULL;
+	func_list[0].sym = "s";
+	func_list[0].f = put_str;
+	func_list[1].sym = "c";
+	func_list[1].f = put_char;
+	func_list[2].sym = "d";
+	func_list[2].f = put_nbr;
+	func_list[3].sym = "i";
+	func_list[3].f = put_nbr;
+	func_list[4].sym = "x";
+	func_list[4].f = call_low_hex;
+	func_list[5].sym = "X";
+	func_list[5].f = call_upp_hex;
+	func_list[6].sym = "u";
+	func_list[6].f = put_unisgned_nbr;
+	func_list[7].sym = "p";
+	func_list[7].f = print_pointer;
+	func_list[8].sym = "%";
+	func_list[8].f = print_space;
+	func_list[9].sym = 0;
+	func_list[9].f = NULL;
 }
 
 int	ft_printf(const char *format, ...)
